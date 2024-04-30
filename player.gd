622a80404a6ddf182952571436750358f7f1d0f5
Fredrik Schön <schon.fredrik@gmail.com>
2024-04-30 21:14:31 +0200

    fix directing being a vector

--- player.gd
+++ player.gd
@@ -14,7 +14,7 @@ func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
